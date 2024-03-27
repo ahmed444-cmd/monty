50555be3297dd5c816460f3de5ec7c852c83112f
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-03-27 15:06:55 -0700

    all

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- none
+  *@head: stck head
+  *@counter: line_numb
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
