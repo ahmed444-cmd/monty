9695a2441ec87d35e021a0007157a6363edaafe6
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-03-27 16:28:24 -0700

    all

--- stck_strng.c
+++ stck_strng.c
@@ -1,44 +1,95 @@
 #include "monty.h"
 
 /**
- * mul_nodes - Adds the top two elements of the stck.
+ * print_char - writes the Ascii value.
  * @stack: Pointer to a pointer of top node of the stck.
  * @line_number: Int representing the ln numb of the opcode.
  */
-void mul_nodes(stack_t **stack, unsigned int line_number)
+void print_char(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int ascii;
+
+	if (stack == NULL || *stack == NULL)
+		string_err(11, line_number);
+
+	ascii = (*stack)->n;
+	if (ascii < 0 || ascii > 127)
+		string_err(10, line_number);
+	printf("%c\n", ascii);
+}
+
+/**
+ * print_str - writes a strng.
+ * @stack: Pointer to a pointer of top node of the stck.
+ * @ln: Int representing the ln numb of the opcode.
+ */
+void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	int ascii;
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		ascii = tmp->n;
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
+
+/**
+ * rotl - Rotates the first nd of the stck to the bottom.
+ * @stack: Pointer to a pointer of top node of the stck.
+ * @ln: Int representing the ln numb of the opcode.
+ */
+void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "mul");
+		return;
 
-	(*stack) = (*stack)->next;
-	sum = (*stack)->n * (*stack)->prev->n;
-	(*stack)->n = sum;
-	free((*stack)->prev);
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
 	(*stack)->prev = NULL;
 }
 
 
 /**
- * mod_nodes - Adds the top two elems of the stck.
+ * rotr - Rotates the first nd of the stck to the top.
  * @stack: Pointer to a pointer of top node of the stck.
- * @line_number: Int representing the ln numb of the opcode.
+ * @ln: Int representing the ln numb of the opcode.
  */
-void mod_nodes(stack_t **stack, unsigned int line_number)
+void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
 {
-	int sum;
+	stack_t *tmp;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
 
-		more_err(8, line_number, "mod");
+	tmp = *stack;
 
+	while (tmp->next != NULL)
+		tmp = tmp->next;
 
-	if ((*stack)->n == 0)
-		more_err(9, line_number);
-	(*stack) = (*stack)->next;
-	sum = (*stack)->n % (*stack)->prev->n;
-	(*stack)->n = sum;
-	free((*stack)->prev);
-	(*stack)->prev = NULL;
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = tmp;
 }
